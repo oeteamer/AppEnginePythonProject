6cadb802b63cbda07a8daec48f82fe56cc64b21a
teamer <oeteamer@gmail.com>
2015-03-02 17:48:44 +0200

    Added update-all and last-updates pages

--- authors/handlers.py
+++ authors/handlers.py
@@ -3,12 +3,13 @@ import models
 
 from core import core_parser
 from core import viewer
+from datetime import datetime
+from google.appengine.api import taskqueue
 
 
 class Index(webapp2.RequestHandler):
     def get(self):
-        authors_model = models.Authors.query()
-        authors_model = authors_model.fetch()
+        authors_model = models.Authors.query().fetch()
         authors = []
         for model in authors_model:
             authors.append({
@@ -23,14 +24,18 @@ class Index(webapp2.RequestHandler):
 
 class Authors(webapp2.RequestHandler):
     def get(self, author_code):
-        author = {'code': author_code, 'name': core_parser.AuthorsParser.get_author_name_from_url(author_code)}
-
-        author_model = models.Authors.get_by_id(author['code'])
+        author_model = models.Authors.get_by_id(author_code)
         if not author_model:
-            models.Authors.create(author).put()
+            author_model = models.Authors.create({
+                'code': author_code,
+                'name': core_parser.AuthorsParser.get_author_name_from_url(author_code)}
+            ).put()
 
-        contents = models.AuthorsBooks.query(ancestor=models.authors_key(author['code'])).order(models.AuthorsBooks.updated_at)
-        contents = contents.fetch()
+        author = {'code': author_model.key.id(), 'name': author_model.name}
+
+        contents = models.AuthorsBooks.query(ancestor=models.authors_key(author['code']))\
+            .order(models.AuthorsBooks.updated_at)\
+            .fetch()
 
         parse_contents = core_parser.AuthorsParser.indexdate_parser(author['code'])
 
@@ -63,65 +68,62 @@ class Books():
         book.volume = item['volume']
         book.put()
 
-    # @staticmethod
-    # def update_books():
-    #     authors = models.Authors.query()
-    #     authors = authors.fetch()
-    #
-    #     for model in authors:
-    #         contents = models.AuthorsBooks.query(ancestor=models.authors_key(model.key.id()))
-
 
 class DatastoreFlush(webapp2.RequestHandler):
     def get(self):
-        authors = models.Authors.query()
-        authors = authors.fetch()
+        authors = models.Authors.query().fetch()
 
         for model in authors:
-            contents = models.AuthorsBooks.query(ancestor=models.authors_key(model.key.id()))
-            contents = contents.fetch()
+            contents = models.AuthorsBooks.query(ancestor=models.authors_key(model.key.id())).fetch()
             for item in contents:
                 item.key.delete()
 
 
 class UpdateBooks(webapp2.RequestHandler):
     def get(self):
-        authors = models.Authors.query()
-        authors = authors.fetch()
+        authors_model = models.Authors.query().fetch()
 
+        authors = []
+        for model in authors_model:
+            author_url = '/author/'+model.key.id()
+            taskqueue.add(url=author_url, method='GET')
+
+            authors.append({
+                'href': author_url,
+                'href_samlib': core_parser.AuthorsParser.get_author_link(model.key.id()),
+                'name': model.name
+            })
+
+        self.response.headers['Content-Type'] = 'text/html'
+        self.response.write(viewer.AuthorsWriter('index.html').write(authors))
+
+
+class LastUpdates(webapp2.RedirectHandler):
+    def get(self):
+        authors = models.Authors.query().fetch()
+        date = datetime.today()
+        date = datetime(date.year, date.month, date.day)
         total_contents = []
         for model in authors:
             author_code = model.key.id()
 
-            author = {'code': author_code, 'name': core_parser.AuthorsParser.get_author_name_from_url(author_code)}
+            author = {'code': author_code, 'name': model.name}
 
-            author_model = models.Authors.get_by_id(author['code'])
-            if not author_model:
-                models.Authors.create(author).put()
+            contents = models.AuthorsBooks.query(ancestor=models.authors_key(author['code']))\
+                .filter(models.AuthorsBooks._properties["updated_at"] > date)\
+                .order(models.AuthorsBooks.updated_at)\
+                .fetch()
 
-            contents = models.AuthorsBooks.query(ancestor=models.authors_key(author['code'])).order(models.AuthorsBooks.updated_at)
-            contents = contents.fetch()
-
-            parse_contents = core_parser.AuthorsParser.indexdate_parser(author['code'])
-
-            for parse_item in parse_contents:
-                book_exsist = False
-                for item in contents:
-                    if parse_item['id'] == item.key.id():
-                        book_exsist = True
-                        if parse_item['volume'] != item.volume:
-                            parse_item['updated'] = item.volume+'->'+parse_item['volume']
-                            total_contents.append({
-                                parse_item
-                            })
-                            Books.update_book(parse_item, author)
-                        continue
-                if not book_exsist:
-                    parse_item['updated'] = '0->'+parse_item['volume']
-                    Books.update_book(parse_item, author)
-                    total_contents.append({
-                        parse_item
-                    })
+            for item in contents:
+                print item
+                total_contents.append({
+                    'author_id': author['code'],
+                    'author': author['name'],
+                    'book': item.book,
+                    'id': item.key.id(),
+                    'href': item.href,
+                    'volume': item.volume
+                })
 
         self.response.headers['Content-Type'] = 'text/html'
-        self.response.write(viewer.AuthorsWriter('books.html').write(total_contents, 'Update!'))
+        self.response.write(viewer.AuthorsWriter('last-updates.html').write(total_contents, 'Updates!'))
--- main.py
+++ main.py
@@ -7,5 +7,6 @@ app = webapp2.WSGIApplication([
     webapp2.Route('/author/<author_code:\w+>', handlers.Authors),
     webapp2.Route('/', handlers.Index),
     webapp2.Route('/datastore_flush', handlers.DatastoreFlush),
-    webapp2.Route('/updateAll', handlers.UpdateBooks),
+    webapp2.Route('/update-all', handlers.UpdateBooks),
+    webapp2.Route('/last-updates', handlers.LastUpdates),
 ], debug=True)
